6ff806bf8f1bb20db47b3020b99173c88ce01dc6
masha14013 <masha14013@rambler.ru>
2023-09-27 11:56:31 +0300

    added file math.fsum, math.prod

---
+++ ex2.py
@@ -0,0 +1,3 @@
+import math
+print(math.fsum([(4 / 1), math.prod([(4 / 3), -1]), (4 / 5), math.prod([(4 / 7), -1]), (4 / 9), math.prod([(4 / 11) * -1]), \
+                 (4 / 13), math.prod([(4 / 15), -1]), (4 / 17), math.prod([(4 / 19), -1])]))
