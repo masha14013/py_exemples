f140ac9c78b0da022f22b368d17ccf0059bd22b0
masha14013 <masha14013@rambler.ru>
2023-09-28 10:49:22 +0300

    added file, map

--- ex2.py
+++ ex2.py
@@ -1,3 +1,2 @@
-import math
-print(math.fsum([(4 / 1), math.prod([(4 / 3), -1]), (4 / 5), math.prod([(4 / 7), -1]), (4 / 9), math.prod([(4 / 11) * -1]), \
-                 (4 / 13), math.prod([(4 / 15), -1]), (4 / 17), math.prod([(4 / 19), -1])]))
+a, b = map(int, input().split())
+print(a, b)
